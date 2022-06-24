d89cc1661fdcaf2d70452923bcb74626142c4314
Sujin <sujin@lifology.com>
2022-06-25 01:40:44 +0530

    Started the Journey

---
+++ Leetcode 217.py
@@ -0,0 +1,9 @@
+class duplicate:
+    def ContainsDupliate(self, nums:List[int]) -> bool:
+
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+              hashset.add(n)
