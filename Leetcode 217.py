6ab52b9ec1bb0e5da33f61a797654addcad01bb0
Sujin <sujin@lifology.com>
2022-06-25 22:23:13 +0530

    done the anagram problem

--- Leetcode 217.py
+++ Leetcode 217.py
@@ -5,5 +5,6 @@ class duplicate:
 
         for n in nums:
             if n in hashset:
-                return True
-              hashset.add(n)
+              return True
+            hashset.add(n)
+              return False
