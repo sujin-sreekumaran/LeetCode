cf1980de96342012d99b57fd883b0224476327e2
sujin-lifology <sujin@lifology.com>
2022-11-30 15:58:23 +0530

    started subsequence strings problem

--- leetcode_1299.py
+++ leetcode_1299.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def replaceElements(self, arr):
-        #initial-max = -1
-        #And the last element arr[] will be the value of arr[n-1] position
-        #iterate the arr through reverse order
-        #everytime new max val will be the oldmax and previous arr[i]
-        
+        # initial-max = -1
+        # And the last element arr[] will be the value of arr[n-1] position
+        # iterate the arr through reverse order
+        # everytime new max val will be the oldmax and previous arr[i]
+
         right = -1
-        for i in range(len(arr) - 1,-1,-1):
-            newMaxValue = max(right,arr[i])
+        for i in range(len(arr) - 1, -1, -1):
+            newMaxValue = max(right, arr[i])
             arr[i] = right
             right = newMaxValue
         return arr
