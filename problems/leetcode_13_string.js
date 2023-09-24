8a7321f4085dca9900679d35630b2a4a218e9036
sujin <sujin@lifology.com>
2023-09-24 17:04:12 +0100

    solved q 13

--- problems/leetcode_13_string.js
+++ problems/leetcode_13_string.js
@@ -1,37 +1,39 @@
-const roman = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-
-const value = "MCMXCIV";
-
 /**
- * Converts a Roman numeral string to an integer value.
+ * Converts a Roman numeral string to an integer.
  *
- * @param {string} value - The Roman numeral string to be converted.
- * @return {number} - The integer value equivalent of the Roman numeral string.
+ * @param {string} s - The Roman numeral string to convert.
+ * @return {number} The integer value of the Roman numeral string.
  */
-const romanToInt = (value) => {
-  let s = value.split("");
+var romanToInt = function (s) {
+  const sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
 
   for (let i = 0; i < s.length; i++) {
-    s[i] = roman[s[i]];
-    if (s[i] != roman.I) {
-      
+    const cur = sym[s[i]];
+    const next = sym[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur;
+      i++;
+    } else {
+      result += cur;
     }
   }
-  const result = s.reduce((a, b) => a + b);
 
-  console.log("====================================");
-  console.log(result);
-  console.log("====================================");
+  console.log("Result >>>", result);
 
   return result;
 };
+
+const value = "XML";
 
 romanToInt(value);
