c2d8cf69847ab0dd8c3a46a2c754976a3dc1ff20
sujin <sujin@lifology.com>
2023-10-23 10:26:12 +0100

    solved array problem

---
+++ problems/leetcode_21.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (!l1) return l2;
+  else if (!l2) return l1;
+  else if (l1.val <= l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+  } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2
+  }
+};
+
+const result = mergeTwoLists([2, 5, 3], [3, 5, 6]);
+console.log(result);
