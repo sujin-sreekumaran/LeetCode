ead533699cbfec26ab2f3df872b7fdaff5b18955
sujin <sujinsr2457@gmail.com>
2025-06-04 12:59:47 +0100

    clear

--- problems/Array/leetcode_1929.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-
-        for i in range(2):
-            for number in nums:
-                ans.append(number) 
-        return ans
--- problems/Array/leetcode_9.js
+++
@@ -1,8 +0,0 @@
-var isPalindrome = function (x) {
-  const reversed = x.toString().split("").reverse().join("");
-  console.log(reversed);
-  return x === +reversed;
-};
-
-const result = isPalindrome(141);
-console.log("result >>>>>>", result);
